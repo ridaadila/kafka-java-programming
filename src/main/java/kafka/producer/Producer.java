46bde5c6d1bbcea1795edc41fc5891e6019311c0
ridaadila <ridaadila10@gmail.com>
2022-09-27 12:56:52 +0700

    add producer class basic

--- src/main/java/kafka/producer/Producer.java
+++ src/main/java/kafka/producer/Producer.java
@@ -1,4 +1,41 @@
 package kafka.producer;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+import java.util.Properties;
+
 public class Producer {
+
+    private static final Logger log = LoggerFactory.getLogger(Producer.class.getSimpleName());
+    public static void main(String[] args) {
+
+        log.info("hello world");
+
+        //create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "10.199.16.93:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        //create producer record
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<>("java_topic", "data dikirim dari program producer java");
+
+        //send data with an asyncrhonous
+        producer.send(producerRecord);
+
+        //flush - synchronous
+        producer.flush();
+
+        //flush and close producer
+        producer.close();
+    }
 }
